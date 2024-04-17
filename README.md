5025c7c1f5bf8d463142642ff40fd5f42f08679d
Dmitriy <dvol72@mail.ru>
2024-04-17 23:15:09 +0300

    fix: card list info

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ссылка на макет - https://www.figma.com/file/6FMWkB94wE7KTkcCgUXtnC/Дипломный-проект?type=design&node-id=932-4503&mode=design&t=ptIzpgKqoVRyHy4Z-0
+
+ссылка на проект - movies-service.nomoredomainswork.ru
+
+https://github.com/WillBeDream/movies-explorer-frontend/pull/2  - пулл реквест
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const ESC_KEY = "Escape";
 
 // API URL
 export const MOVIES_API_URL = "https://api.nomoreparties.co";
-export const MAIN_API_URL = "https://api.movies-service.nomoredomainswork.ru";
+export const MAIN_API_URL = "http://localhost:3000";
 
 // REG EXP
 export const USER_NAME_REG_EXP = "^[A-Za-zА-Яа-яЁё\\-\\s]+$";
